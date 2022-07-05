fa978f842f9732ce63cef1c82e2ec15725b6a50a
Matthew Kevins <mmkevins@yahoo.com>
2022-07-05 16:49:40 +1000

    Add navigator to globalThis
    This gets us a little farther along, but we run into an error for
    matchMedia (not a function) on the global window object. That will be up
    next.

--- globals.js
+++ globals.js
@@ -2,7 +2,8 @@ import {JSDOM} from 'jsdom';
 
 const dom = new JSDOM('<!DOCTYPE html>');
 const {window} = dom;
-const {document} = window;
+const {document, navigator} = window;
 
 globalThis.window = window;
 globalThis.document = document;
+globalThis.navigator = navigator;
