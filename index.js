0d50ecbacd8081faf2d8511b48a284c8d8d30b5f
Matthew Kevins <mmkevins@yahoo.com>
2022-07-05 16:57:47 +1000

    Import createBlock, create, and log a paragraph block
    Tada! We have command line blocks! - Next up: serializing them. :)

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import './globals.js';
 import {registerCoreBlocks} from '@wordpress/block-library';
+import {createBlock} from '@wordpress/blocks';
 
 registerCoreBlocks();
+
+const block = createBlock('core/paragraph', {
+  content: "Hello blocks!",
+});
+
+console.log(block);
