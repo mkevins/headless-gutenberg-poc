70e032f6665ce992a80cbb6fa93a083ec5f0978c
Matthew Kevins <mmkevins@yahoo.com>
2022-07-05 16:52:35 +1000

    Add fake matchMedia function
    With this, we can finally import and register core blocks from the
    `@wordpress/block-library` package. :)

--- globals.js
+++ globals.js
@@ -7,3 +7,11 @@ const {document, navigator} = window;
 globalThis.window = window;
 globalThis.document = document;
 globalThis.navigator = navigator;
+
+// add fake matchMedia
+window.matchMedia = window.matchMedia
+  || (() => ({
+    matches: false,
+    addListener: () => {},
+    removeListener: () => {},
+  }));
