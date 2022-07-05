210d0f3ee84288a7d1b19b6fa607c6edf15baf49
Matthew Kevins <mmkevins@yahoo.com>
2022-07-05 16:48:34 +1000

    Add document to globalThis
    This still is not enough, as we get an error for navigator.

--- globals.js
+++ globals.js
@@ -2,5 +2,7 @@ import {JSDOM} from 'jsdom';
 
 const dom = new JSDOM('<!DOCTYPE html>');
 const {window} = dom;
+const {document} = window;
 
 globalThis.window = window;
+globalThis.document = document;
