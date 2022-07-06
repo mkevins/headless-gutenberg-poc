d170bc3738cfa99a5b49d7e0cdcb15e71f823c6b
Matthew Kevins <mmkevins@yahoo.com>
2022-07-06 12:58:33 +1000

    Generate paragraph blocks with alternating alignment
    This demonstrates that the proper CSS class is also assigned.

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ import {createBlock, serialize} from '@wordpress/blocks';
 
 registerCoreBlocks();
 
-const block = createBlock('core/paragraph', {
-  content: "Hello blocks!",
-});
+const blocks = Array.from({length: 5}, (e, i) => (
+  createBlock('core/paragraph', {
+    content: `Hello block number ${i + 1}!`,
+    align: i % 2 == 0 ? 'left' : 'right',
+  })
+));
 
-const output = serialize(block);
+const output = serialize(blocks);
 
 process.stdout.write(output);
 process.stdout.write('\n');
